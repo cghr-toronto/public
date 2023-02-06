71c9477bcbb616fbf01d74ab72061156dc11e6f0
hgw0610209 <48361900+hgw0610209@users.noreply.github.com>
2023-02-06 17:16:12 -0500

    Update plots.R

--- PollutionHealth47CDs/plots.R
+++ PollutionHealth47CDs/plots.R
@@ -13,18 +13,18 @@ cdShp <- cdShp %>% dplyr::left_join(CD_fit_sep_name, by=c("CDUID"))
 cdShp <- cdShp[which(cdShp$name==1),]
 
 
-obtain_nice_data <- function(input_model, pollutant, xlim=c(0.92,1.06),nationalLine)
+obtain_nice_data <- function(input_model, pollutant, xlim=c(0.9,1.1),nationalLine)
 {
   
-  est <- exp(mapply(function(x){x$coefficients[1:3]}, input_model)*c(8.3,14,14.4))
+  est <- exp(mapply(function(x){x$coefficients[1:3]}, input_model)*c(7.6, 12.8, 14))
   
   se <- mapply(function(x){diag(x$var)[1:3]}, input_model)
   
   lower <- exp(mapply(function(x){x$coefficients[1:3]- 1.96*(sqrt(diag(x$var)[1:3]))
-  }, input_model)*c(8.3,14,14.4))
+  }, input_model)*c(7.6, 12.8, 14))
   
   upper <- exp(mapply(function(x){x$coefficients[1:3]+ 1.96*(sqrt(diag(x$var)[1:3]))
-  }, input_model)*c(8.3,14,14.4))
+  }, input_model)*c(7.6, 12.8, 14))
   
   rownames(se) <- rownames(est)
   x = data.frame(CD=c(CD_fit_sep), est=est[pollutant,], se_raw=se[pollutant,], lower=lower[pollutant,], upper=upper[pollutant,])
@@ -53,9 +53,15 @@ obtain_nice_data <- function(input_model, pollutant, xlim=c(0.92,1.06),nationalL
   abline(v=nationalLine, col='black')
   segments(x$lower, x$index, x$upper, x$index, pch=15, col=col2html(x$colLine,x$cex))
   x$cityName=""
-  x$cityName[x$CD==3520] <- "Toronto"
-  x$cityName[x$CD==3525] <- "Hamilton"
-  x$cityName[x$CD==5915] <- "Vancouver"
+x$cityName[x$CD==3506] <- "Ottawa"
+x$cityName[x$CD==3518] <- "Durham"
+x$cityName[x$CD==3519] <- "York"
+x$cityName[x$CD==3520] <- "Toronto"
+x$cityName[x$CD==3521] <- "Peel"
+x$cityName[x$CD==3524] <- "Halton"
+x$cityName[x$CD==3525] <- "Hamilton"
+x$cityName[x$CD==3530] <- "Waterloo"
+x$cityName[x$CD==5915] <- "Vancouver"
   text(
     x$textloc, 
     x$index, x$cityName,
@@ -73,8 +79,8 @@ model_result_morb <- model_result
 load("combine_result_mort_all-causeall-ageall-yearS.RData")
 load("combine_result_morb_all-causeall-ageall-yearS.RData")
 
-national_mort <- exp(apply(combine_result_mort$betaBar,2,median)*c(8.3,14,14.4))
-national_morb <- exp(apply(combine_result_morb$betaBar,2,median)*c(8.3,14,14.4))
+national_mort <- exp(apply(combine_result_mort$betaBar,2,median)*c(7.6, 12.8, 14))
+national_morb <- exp(apply(combine_result_morb$betaBar,2,median)*c(7.6, 12.8, 14))
 
 obtain_nice_data(model_result_mort,"pm25",nationalLine=national_mort[1])
 # obtain_nice_data(model_result_morb,"pm25",nationalLine=national_morb[1])
